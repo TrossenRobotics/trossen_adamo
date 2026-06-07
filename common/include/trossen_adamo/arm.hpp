c137e94989814b6cf63826afef8e577ec73b8ff4
abhi chothani <achothani42@gmail.com>
2026-06-07 18:38:01 -0500

    update cmake, add docs, update home position

--- common/include/trossen_adamo/arm.hpp
+++ common/include/trossen_adamo/arm.hpp
@@ -16,7 +16,7 @@ namespace trossen_adamo::arm {
 
 // Home pose: 7 joints with both shoulder joints at pi/2 and the rest zero.
 inline std::vector<double> home_pose() {
-    return {0.0, M_PI_2, M_PI_2, 0.0, 0.0, 0.0, 0.0};
+    return {0.0, M_PI/3, M_PI/6, M_PI/5, 0.0, 0.0, 0.0};
 }
 
 inline std::unique_ptr<trossen_arm::TrossenArmDriver>
